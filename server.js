4b6c4d03c8a0c40138ce5b13d69a6da1539e6e06
Thotsapon Nuanchuay <ntossapo@gmail.com>
2016-02-18 21:36:32 +0700

    insert or update data into mysql

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var mysql = require('mysql');
 var config = require('./config');
 
 //var url = 'mongodb://localhost:27017/stadium';
-var port = 3333;
+var port = 3000;
 var connection = mysql.createConnection(config.mysql);
 
 function update(facebookId, lat, lng, callback){
@@ -21,15 +21,23 @@ function insert(facebookId, lat, lng, callback){
 	connection.query('insert into user_location(facebook_id, latitude, longitude) values(?, ?, ?)', [facebookId, lat, lng], callback);
 }
 
+var callback = function(err, result){
+}
+
+
+connection.connect();
 io.on('connection', function(socket){
 	socket.on('location', function(msg){
-		connection.connect();
-		var userLocaiton = JSON.parse(msg);	
-		connection.query('select count(id) from user_location where facebook_id = ?', [userLocation.user], function(err, result){
-			console.log(result);
+		console.log(msg);
+		var userLocation = JSON.parse(msg);	
+		connection.query('select count(id) as count from user_location where facebook_id = ?', [userLocation.user], function(err, result){
+			if(result[0].count == 0){
+				insert(userLocation.user, userLocation.lat, userLocation.lng, callback);
+			}else{
+				update(userLocation.user, userLocation.lat, userLocation.lng, callback);
+			}
 		});
-
-		connection.end();		
+		
 	});
 });
 
