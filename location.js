6b26443b9340a63399f8cd9152c77eb000ef2cf8
Tossapon Nuanchuay <ntossapo@gmail.com>
2016-02-21 04:24:29 -0500

    add location.js

---
+++ location.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+//var MongoClient = require('mongodb').MongoClient
+//, assert = require('assert');
+var mysql = require('mysql');
+
+//var mongo = require('./mongo');
+var config = require('./config');
+
+//var url = 'mongodb://localhost:27017/stadium';
+var port = 3000;
+var connection = mysql.createConnection(config.mysql);
+
+function update(facebookId, lat, lng, callback){
+	connection.query('update user_location set latitude=?, longitude=? where facebook_id=?', [lat, lng, facebookId], callback);
+}
+
+function insert(facebookId, lat, lng, callback){
+	connection.query('insert into user_location(facebook_id, latitude, longitude, last_used) values(?, ?, ?, ?)', [facebookId, lat, lng, new Date()], callback);
+}
+
+var callback = function(err, result){
+}
+
+
+connection.connect();
+io.on('connection', function(socket){
+	socket.on('location', function(msg){
+		console.log(msg);
+		var userLocation = JSON.parse(msg);	
+		connection.query('select count(id) as count from user_location where facebook_id = ?', [userLocation.user], function(err, result){
+			if(result[0].count == 0){
+				insert(userLocation.user, userLocation.lat, userLocation.lng, callback);
+			}else{
+				update(userLocation.user, userLocation.lat, userLocation.lng, callback);
+			}
+		});
+		
+	});
+});
+
+http.listen(port, function(){
+	console.log("server at port " + port);
+});
