bc59eef1c38e2b6064321d10a6c91dbf302bb209
Thotsapon Nuanchuay <ntossapo@gmail.com>
2016-02-21 16:44:09 +0700

    add notification service

---
+++ notification.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+//var MongoClient = require('mongodb').MongoClient
+//, assert = require('assert');
+
+//var mongo = require('./mongo');
+var config = require('./config');
+
+//var url = 'mongodb://localhost:27017/stadium';
+var port = 9990;
+
+
+
+io.on('connection', function(socket){
+	socket.on('notification_center', function(msg){
+		socket.emit('notification', msg);		
+	});
+});
+
+http.listen(port, function(){
+	console.log("server at port " + port);
+});
